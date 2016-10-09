cc07c3122559adfde3e2486b36f74ac62aa62574
Matt Rice <mattrice12@outlook.com>
2016-10-08 21:11:21 -0500

    converted nav bar into React component

---
+++ app/assets/javascripts/components/nav.js.jsx
@@ -0,0 +1,11 @@
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <div className="content">
+        <a className="title is-3" href="/users">Friends</a>
+        <a className="title is-3" href="/games">Games</a>
+        <a className="title is-3" href="/categories">Categories</a>
+      </div>
+    )
+  }
+})
