453326ad6987c30142869ac50692a761aaa6ea11
Matt Rice <mattrice12@outlook.com>
2016-10-10 19:26:09 -0500

    added edit feature and styling for users

--- app/assets/javascripts/components/User.js.jsx
+++ app/assets/javascripts/components/User.js.jsx
@@ -1,6 +1,10 @@
 var User = React.createClass({
   render: function(){
     return(<div className="content">
+      <a href={"/users/" + this.props.user.id + "/edit"} className="edit button is-primary">Edit User</a>
+      <div className="photo">
+        <img src={this.props.user.avatar_url_med} alt={this.props.user.username} title={this.props.user.username}/>
+      </div>
       <h1 className="title is-1 user">{this.props.user.username}</h1>
 
       <div className="columns">
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,20 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: { maximum: 128}
 
-  has_attached_file :avatar, styles: { medium: "100x100>", thumb: "50x50>" }, default_url: "http://headphonespares.sennheiser.co.uk/gfx/default-missing-category-placeholder.png"
+  has_attached_file :avatar, styles: { medium: "250x250>", thumb: "50x50>" }, default_url: "http://headphonespares.sennheiser.co.uk/gfx/default-missing-category-placeholder.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def avatar_url_med
+    avatar.url(:medium)
+  end
 
   def avatar_url_thumb
     avatar.url(:thumb)
   end
 
   def as_json(_= nil)
-    super(include: :games, methods: :avatar_url_thumb)
+    super(include: :games, methods: [:avatar_url_thumb, :avatar_url_med])
   end
 
 end
