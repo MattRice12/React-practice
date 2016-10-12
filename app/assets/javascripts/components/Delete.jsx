7a0aa638b6adc837991d4b753d0f00458e3b9a7a
Matt Rice <mattrice12@outlook.com>
2016-10-12 18:18:03 -0500

    customized delete message to distinguish between ending friendships and deleting other objects such as games

--- app/assets/javascripts/components/Delete.jsx
+++ app/assets/javascripts/components/Delete.jsx
@@ -1,6 +1,6 @@
 var Delete = React.createClass({
+
   handleDelete: function (e) {
-    console.log(this.props)
     $.ajax({
       url: "/" + this.props.url + "/" + this.props.obj.id,
       method: "DELETE",
@@ -8,9 +8,12 @@ var Delete = React.createClass({
   },
 
   render: function() {
+    let deleteMessage = "Delete";
+    if (this.props.url === "friendships") {
+      deleteMessage = "End Friendship"
+    };
     return (
-      <a onClick={this.handleDelete} className="button is-small is-danger is-outlined react-clear" title="Delete User">Delete
-      </a>
+      <a onClick={this.handleDelete} className="button is-small is-danger is-outlined react-clear" title="Delete">{deleteMessage}</a>
     )
   }
 });
