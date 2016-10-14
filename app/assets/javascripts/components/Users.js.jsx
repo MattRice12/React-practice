8dd8fd19f46e6c5f9f8baf024ccea44f5b29f573
Matt Rice <mattrice12@outlook.com>
2016-10-14 15:37:43 -0500

    refactoring components further

--- app/assets/javascripts/components/Users.js.jsx
+++ app/assets/javascripts/components/Users.js.jsx
@@ -1,25 +1,28 @@
 var Users = React.createClass({
   render: function() {
+    let canDelete;
     let currentUser = this.props.current_user;
+
+    let eachUser = this.props.users.map(function(user) {
+      if (currentUser.admin === true) {
+        canDelete = <Delete obj={user}
+                url={"users"}/>;
+      }
+      return(
+        <div key={user.id} className="obj-with-del">
+          <ObjectList obj={user}
+                      url={"users"}/>
+          {canDelete}
+          <AddFriend user={currentUser} friend={user}/>
+        </div>
+      )
+    });
+
     return(
-      <div className="content">
-        <h1 className="title is-1 user"> All Users: </h1>
+      <div>
+        <h1 className="title is-1"> All Users: </h1>
         <div>
-          {this.props.users.map(function(user) {
-            if (currentUser.admin === true) {
-              canDelete = <Delete obj={user}
-                      url={"users"}/>;
-            }
-            return (
-              <div key={user.id} className="obj-with-del">
-                <ObjectList obj={user}
-                            url={"users"}/>
-                {canDelete}
-                <AddFriend user={currentUser} friend={user}/>
-              </div>
-              )
-            })
-          }
+          {eachUser}
         </div>
       </div>
     )
